3d886981f221d86188fbedf9d86d6f02a71568ea
luguanrui <1481744045@qq.com>
2018-04-18 15:50:30 +0800

    标的列表分页

--- src/components/invest/bid-list/bid-list.vue
+++ src/components/invest/bid-list/bid-list.vue
@@ -4,6 +4,14 @@
     <div class="invest-list-content">
       <BidExp :expList="expList"></BidExp>
       <BidItem :bidList="bidList"></BidItem>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        background
+        layout="prev, pager, next"
+        :page-size="10"
+        :total="total">
+      </el-pagination>
     </div>
     <Footer></Footer>
   </div>
@@ -20,27 +28,45 @@
       return {
         title: '投资列表',
         expList: [],
-        bidList: []
+        bidList: [],
+        total: 0
       }
     },
     mounted(){
-      const data = {
-        aprSearch: '',
-        nameSearch: '',
-        pageNo: 1,
-        pageSize: 10,
-        statusSearch: '',
-        timeSearch: '',
-        typeSearch: ''
-      }
-      getData('/invest/list', '', data).then(res => {
-        if (res.data.code === '0' && res.data.success) {
-          const list = res.data.data.list
-          this.expList = list.slice(0, 1)
-          this.bidList = list.slice(1)
-          this.bidList[6].name = '热门标的'
+      this._getData(1, 10)
+    },
+    methods: {
+      _getData(pageNo, pageSize){
+        const data = {
+          aprSearch: '',
+          nameSearch: '',
+          pageNo: pageNo,
+          pageSize: pageSize,
+          statusSearch: '',
+          timeSearch: '',
+          typeSearch: ''
         }
-      })
+        getData('/invest/list', '', data).then(res => {
+          if (res.data.code === '0' && res.data.success) {
+            const list = res.data.data.list
+            if (list[0].type === '300') {
+              this.expList = list.slice(0, 1)
+              this.bidList = list.slice(1)
+              this.bidList[6].name = '热门标的'
+            } else {
+              this.expList = []
+              this.bidList = list
+            }
+            this.total = res.data.data.totalCount
+          }
+        })
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`)
+      },
+      handleCurrentChange(val) {
+        this._getData(val, 10)
+      }
     },
     components: {
       Header,
@@ -76,4 +102,9 @@
             svg
               .el-progress-circle__path
                 stroke: #f50
+      .el-pagination
+        margin-top: 12px
+        text-align: right
+        .el-pager li
+          min-width: 21.5px
 </style>
